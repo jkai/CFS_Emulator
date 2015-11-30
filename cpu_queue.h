5e5374d08cf53e8e50a20176bf17ad304ac9a328
Junjie Kai <opendoublej@gmail.com>
2015-11-30 18:09:06 -0500

    Create cpu_queue.h

---
+++ cpu_queue.h
@@ -0,0 +1,13 @@
+/* 
+SYSC4001 Assignment3
+Student: Junjie Kai	100814819
+*/
+
+#ifndef Assignment3_cpu_queue_h
+#define Assignment3_cpu_queue_h
+
+#include "Common.h"
+
+void multilevel_queue_init(multilevel_queue* mq);
+
+#endif
